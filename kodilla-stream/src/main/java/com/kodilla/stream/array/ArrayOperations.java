b0f646124b9e8e578f2d56e4fe433360978775bf
Rafal Witkowski <rdna2@wp.pl>
2022-01-24 17:09:51 +0100

    7.5 Added kodilla-stream module

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,9 +1,9 @@
 package com.kodilla.stream.array;
 
-
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
+
     static double getAverage(int[] numbers){
 
       IntStream.range(0, numbers.length)
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,10 +1,11 @@
-package stream.array;
+package com.kodilla.stream.array;
 
 import com.kodilla.stream.array.ArrayOperations;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ArrayOperationsTestSuite {
+
     @Test
     public void testGetAverage(){
         //Given
