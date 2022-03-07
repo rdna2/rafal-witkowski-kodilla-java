e2821702806810689fd7868c7543b55903cac2ff
Rafal Witkowski <rdna2@wp.pl>
2022-03-07 06:29:16 +0100

    13.1 Added kodilla-good-patterns module

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,40 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class MovieStore {
+
+        public Map<String, List<String>> getMovies() {
+            List<String> ironManTranslations = new ArrayList<>();
+            ironManTranslations.add("Żelazny Człowiek");
+            ironManTranslations.add("Iron Man");
+
+            List<String> avengersTranslations = new ArrayList<>();
+            avengersTranslations.add("Mściciele");
+            avengersTranslations.add("Avengers");
+
+            List<String> flashTranslations = new ArrayList<>();
+            flashTranslations.add("Błyskawica");
+            flashTranslations.add("Flash");
+
+            Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+            booksTitlesWithTranslations.put("IM", ironManTranslations);
+            booksTitlesWithTranslations.put("AV", avengersTranslations);
+            booksTitlesWithTranslations.put("FL", flashTranslations);
+
+            return booksTitlesWithTranslations;
+        }
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        System.out.println(movieStore.getMovies().values().stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.joining(" ! ")));
+
+
+    }
+
+
+    }
+
