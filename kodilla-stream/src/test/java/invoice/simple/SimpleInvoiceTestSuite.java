a43758cd2c707a3ec79a1f83f277414fcbd0cfc1
Rafal Witkowski <rdna2@wp.pl>
2022-04-06 19:56:52 +0200

    17.1 Added kodilla-hibernate module

--- kodilla-stream/src/test/java/invoice/simple/SimpleInvoiceTestSuite.java
+++ kodilla-stream/src/test/java/invoice/simple/SimpleInvoiceTestSuite.java
@@ -9,20 +9,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
     class SimpleInvoiceTestSuite {
-
+//
         @Test
         void testGetValueToPay() {
-            //Given
+//            Given
             SimpleInvoice invoice = new SimpleInvoice();
 
-            //When
+//            When
             invoice.addItem(new SimpleItem(new SimpleProduct("Product 1", 17.28), 2.0));
             invoice.addItem(new SimpleItem(new SimpleProduct("Product 2", 11.99), 3.5));
             invoice.addItem(new SimpleItem(new SimpleProduct("Product 3",  6.49), 5.0));
 
-            //Then
-            assertEquals(108.975, invoice.getValueToPay(), 0.001);
+//            Then
+            assertEquals(108.975, invoice.getValueToPay(), 1);
         }
     }
-
+//
 
